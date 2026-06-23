346b0630d61eb9877b282a07845b785559564684
Arsenii Turbabin <aturbabin@burut.org>
2026-06-23 18:19:13 +0300

    refactor(catalog/models): update catalog models

--- test_store/catalog/models/category.py
+++ test_store/catalog/models/category.py
@@ -2,11 +2,16 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(
+        verbose_name="Name",
+        max_length=255,
+        unique=True,
+    )
 
     class Meta:
-        verbose_name = "category"
-        verbose_name_plural = "categories"
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ("name",)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
--- test_store/catalog/models/good.py
+++ test_store/catalog/models/good.py
@@ -4,17 +4,26 @@ from .category import Category
 
 
 class Good(models.Model):
-    name = models.CharField(max_length=255)
-
+    name = models.CharField(
+        max_length=255,
+    )
     category = models.ForeignKey(
         Category,
         on_delete=models.PROTECT,
         related_name="goods",
     )
-
     price = models.DecimalField(
         max_digits=12,
         decimal_places=2,
     )
+    promo_excluded = models.BooleanField(
+        default=False,
+    )
 
-    promo_excluded = models.BooleanField(default=False)
+    class Meta:
+        verbose_name = "Good"
+        verbose_name_plural = "Goods"
+        ordering = ("name",)
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.price})"
